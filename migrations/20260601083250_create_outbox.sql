b373b5ff8c91b98a288fa647317253269a4f3984
au <ustyuzhaninov@gmail.com>
2026-06-03 18:46:05 +0500

    fix: empty migration

--- migrations/20260601083250_create_outbox.sql
+++ migrations/20260601083250_create_outbox.sql
@@ -1,5 +1,23 @@
 -- +goose Up
-SELECT 'up SQL query';
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS outbox (
+                                      id           UUID         PRIMARY KEY DEFAULT gen_random_uuid(),
+    event_type   VARCHAR(100) NOT NULL,
+    payload      JSONB        NOT NULL,
+    status       VARCHAR(20)  NOT NULL DEFAULT 'pending',
+    created_at   TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+    processed_at TIMESTAMPTZ,
+
+    CONSTRAINT outbox_status_check
+    CHECK (status IN ('pending', 'processed', 'failed'))
+    );
+
+CREATE INDEX IF NOT EXISTS idx_outbox_status_created
+    ON outbox (status, created_at)
+    WHERE status = 'pending';
+-- +goose StatementEnd
 
 -- +goose Down
-SELECT 'down SQL query';
+-- +goose StatementBegin
+DROP TABLE IF EXISTS outbox;
+-- +goose StatementEnd
